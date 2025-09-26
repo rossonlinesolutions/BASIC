ea1a83a3de280a3bb8ed56f7771317383538ed32
Adrian Roß <adrian.ross@ross-agentur.de>
2025-09-26 20:20:14 +0200

    Modularize main

--- src/Main.cpp
+++ src/Main.cpp
@@ -2,25 +2,6 @@
 
 #include <iostream>
 
-int runFile(char* filename) {
-    BasicFileConsole console {filename};
-    BasicInterpreter interpreter {console};
-    interpreter.runInteractive();
-    return 0;
-}
-
 int main(int argc, char** argv) {
-    if(argc > 2) {
-        return runFile(argv[1]);
-    }
-
-    std::cout << "Ross Online Solutions Tiny BASIC (main)" << std::endl;
-    std::cout << "Version 1.0.0" << std::endl;
-
-    // else run interactive
-    BasicTerminal terminal;
-    BasicInterpreter interpreter {terminal};
-
-    interpreter.runInteractive();
-    return 0;
+    return basic::run(argc, argv, nullptr);
 }
--- src/MainModule.cpp
+++ src/MainModule.cpp
@@ -18,3 +18,30 @@
 #include "BasicToken.cpp"
 #include "BasicUnaryExpression.cpp"
 #include "BasicVarExpression.cpp"
+
+namespace basic {
+
+    static int basic_runFile(char* filename) {
+        BasicFileConsole console {filename};
+        BasicInterpreter interpreter {console};
+        interpreter.runInteractive();
+        return 0;
+    }
+
+    int run(int argc, char** argv, BasicConsole* console) {
+        if(argc > 2) {
+            return basic_runFile(argv[1]);
+        }
+
+        std::cout << "Ross Online Solutions Tiny BASIC (main)" << std::endl;
+        std::cout << "Version 1.0.0" << std::endl;
+
+        // else run interactive
+        BasicTerminal terminal;
+        BasicInterpreter interpreter {console ? *console : terminal};
+
+        interpreter.runInteractive();
+        return 0;
+    }
+
+}
